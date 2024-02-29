ee157ef8f29a73492ad153d888b7b2ec3c5c34c9
Prince Kumar <neo11prince@gmail.com>
2024-02-29 17:18:51 +0530

    create repoitems component

---
+++ src/components/RepoItem/RepoItem.jsx
@@ -0,0 +1,143 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Octicon, {
+    Repo,
+    Star,
+    RepoForked,
+    Calendar,
+    Sync,
+    Eye,
+    Browser,
+    IssueOpened,
+} from '@primer/octicons-react';
+import './ReposItemStyles.scss';
+
+// Class component used in order for FlipMove animations to work
+class RepoItem extends React.Component {
+    calculateSize = () => {
+        const {
+            repo: { size },
+        } = this.props;
+
+        if (size > 1000) {
+            return `${size / 1000} MB`;
+        }
+        return `${size} KB`;
+    };
+
+    getDescription = () => {
+        const {
+            repo: { description },
+        } = this.props;
+
+        const descriptionChars = description.split('');
+        const descriptionLength = 140;
+        if (descriptionChars.length > descriptionLength) {
+            const newDescription = descriptionChars.slice(0, descriptionLength);
+            if (newDescription[descriptionLength - 1].match(/\W$/gm)) {
+                newDescription.pop();
+            }
+            return `${newDescription.join('').trim()}...`;
+        }
+        return description.trim();
+    };
+
+    render = () => {
+        const {
+            repo: {
+                name,
+                description,
+                html_url,
+                created_at,
+                updated_at,
+                forks_count,
+                stargazers_count,
+                watchers_count,
+                language,
+                open_issues_count,
+            },
+        } = this.props;
+
+        return (
+            <a
+                href={html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="repo-github-url">
+                <li className="repo-item">
+                    <div className="repo-item__name">
+                        <Octicon icon={Repo} />
+                        <h3>{name}</h3>
+                    </div>
+                    {description && (
+                        <p className="repo-item__description">{this.getDescription()}</p>
+                    )}
+                    <div className="repo-item__attributes repo-item__attributes--1">
+                        <div className="repo-item__attributes__created_at">
+                            <Octicon icon={Calendar} size="small" />
+                            Created:{' '}
+                            {new Date(created_at).toLocaleDateString('en-UK', {
+                                day: 'numeric',
+                                month: 'numeric',
+                                year: 'numeric',
+                            })}
+                        </div>
+                        <div className="repo-item__attributes__updated_at">
+                            <Octicon icon={Sync} size="small" />
+                            Updated:{' '}
+                            {new Date(updated_at).toLocaleDateString('en-UK', {
+                                day: 'numeric',
+                                month: 'numeric',
+                                year: 'numeric',
+                            })}
+                        </div>
+                    </div>
+                    <div className="repo-item__attributes repo-item__attributes--2">
+                        {language && (
+                            <div className="repo-item__attributes__language">
+                                <Octicon icon={Browser} size="small" />
+                                {language}
+                            </div>
+                        )}
+                        <div className="repo-item__attributes__stars">
+                            <Octicon icon={Star} size="small" />
+                            {stargazers_count}
+                        </div>
+                        <div className="repo-item__attributes__forks">
+                            <Octicon icon={RepoForked} size="small" />
+                            {forks_count}
+                        </div>
+                        <div className="repo-item__attributes__issues">
+                            <Octicon icon={IssueOpened} size="small" />
+                            {open_issues_count}
+                        </div>
+                        <div className="repo-item__attributes__watching">
+                            <Octicon icon={Eye} size="small" />
+                            {watchers_count}
+                        </div>
+                        <div className="repo-item__attributes__size">{this.calculateSize()} </div>
+                    </div>
+                </li>
+            </a>
+        );
+    };
+}
+
+RepoItem.propTypes = {
+    repo: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        html_url: PropTypes.string.isRequired,
+        created_at: PropTypes.string.isRequired,
+        updated_at: PropTypes.string.isRequired,
+        language: PropTypes.string,
+        size: PropTypes.number.isRequired,
+        forks_count: PropTypes.number.isRequired,
+        stargazers_count: PropTypes.number.isRequired,
+        watchers_count: PropTypes.number.isRequired,
+        open_issues_count: PropTypes.number.isRequired,
+    }).isRequired,
+};
+
+export default RepoItem;
